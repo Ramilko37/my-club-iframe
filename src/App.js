2be5f8b6868bf16baed9aa6ab6467c44f952dc96
ramilko37 <galyamdin@gmail.com>
2025-05-05 14:27:56 +0300

    post

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import {
-  Button,
-  ChakraProvider,
-  Flex,
-  theme
-} from '@chakra-ui/react';
+import { Button, ChakraProvider, Flex, theme } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { base64img } from './constant.js';
 
 function App() {
-
-  const handleBtnClick = () => { 
-    const iframeWindow = document.getElementById('iframe').contentWindow
-    iframeWindow.postMessage({ type: 'initialImage', payload: base64img}, '*')
-  }
+  const handleBtnClick = () => {
+    const iframeWindow = document.getElementById('iframe').contentWindow;
+    iframeWindow.postMessage({ type: 'initialImage', payload: base64img }, '*');
+  };
 
   useEffect(() => {
     window.addEventListener('message', function (event) {
-      if(event.origin !== 'https://dev.myclub.asdf.superlook.ai/') {
-        return
+      if (event.origin !== 'https://myclub.otcgmj.superlook.ai/') {
+        return;
       }
 
-      this.localStorage.setItem('result', event.data)
-      let image = this.document.createElement('img')
-      image.width = '100vw'
-      image.height = '100vh'
-      image.src = event.data
-      this.document.body.appendChild(image)
-    })
-  }, [])
+      this.localStorage.setItem('result', event.data);
+      let image = this.document.createElement('img');
+      image.width = '100vw';
+      image.height = '100vh';
+      image.src = event.data;
+      this.document.body.appendChild(image);
+    });
+  }, []);
 
   return (
     <ChakraProvider theme={theme}>
       <Flex w={'100%'} h={'100dvh'} textAlign="center" fontSize="xl">
-         <Button pos={'absolute'} top={'5%'} left={'50%'} transform={'translate(-50%, 0)'}  onClick={handleBtnClick}>Send Message</Button>  
-         <iframe style={{ width: '100%', height: '100%' }}  id='iframe' src='https://dev.myclub.asdf.superlook.ai/' />
-         
+        <Button
+          pos={'absolute'}
+          top={'5%'}
+          left={'50%'}
+          transform={'translate(-50%, 0)'}
+          onClick={handleBtnClick}
+        >
+          Send Message
+        </Button>
+        <iframe
+          style={{ width: '100%', height: '100%' }}
+          id="iframe"
+          src="https://myclub.otcgmj.superlook.ai/"
+        />
       </Flex>
     </ChakraProvider>
   );
